6a58eeb86d7d99f5a20ce52db354161a79270d2f
Janelin126 <79820235+Janelin126@users.noreply.github.com>
2022-09-06 13:21:54 +1200

    name input testing

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
+ 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        name = request.form["name"]
+        return redirect(url_for('generate', name=name))
     return render_template('index.html')
 
 
-@app.route('/generate')
+@app.route('/generate', methods=['GET','POST'])
 def generate():
-    return render_template('generate.html')
+    name = request.form.get('name')
+    return render_template('generate.html', name=name)
 
 
 @app.route('/reference')
 def reference():
     return render_template('reference.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
