409093d20278aa11ca49e66c2052307b7887b3ea
Janelin126 <79820235+Janelin126@users.noreply.github.com>
2022-09-06 23:23:41 +1200

    end

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, g
+import sqlite3
  
 app = Flask(__name__)
 
+MENUDB = 'dob.db'
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    db = sqlite3.connect(MENUDB)
+    print(db)
     if request.method == 'POST':
         name = request.form["name"]
         return redirect(url_for('generate', name=name))
@@ -16,8 +21,13 @@ def generate():
     return render_template('generate.html', name=name)
 
 
-@app.route('/reference')
+@app.route('/reference', methods=['GET','POST'])
 def reference():
+    db = sqlite3.connect(MENUDB)
+    print(db)
+    cur = conn.cursor()
+    cur.execute("SELECT title FROM poems")
+    rows = cur.
     return render_template('reference.html')
 
 if __name__ == "__main__":
