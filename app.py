c521edaeb02bbdb2f8fc4bb0b360a1a42b522518
Janelin126 <79820235+Janelin126@users.noreply.github.com>
2022-09-12 11:53:25 +1200

    db file connecting

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from sqlite3 import dbapi2
 from flask import Flask, render_template, request, url_for, redirect, g
 import sqlite3
  
@@ -7,20 +8,25 @@ MENUDB = 'dob.db'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    db = sqlite3.connect(MENUDB)
-    print(db)
-    if request.method == 'POST':
+     if request.method == 'POST':
         name = request.form["name"]
-        return redirect(url_for('generate', name=name))
-    return render_template('index.html')
+        return redirect(url_for('generate', name=name)) # Is name=name needed here?
+     return render_template('index.html')
 
-
+# request function for image category input.
 @app.route('/generate', methods=['GET','POST'])
 def generate():
+    if request.method == 'POST':
+       imgcate = request.form['']
+       return redirect(url_for('reference'))
+
     name = request.form.get('name')
     return render_template('generate.html', name=name)
+    
 
 
+#import db for poem section,
+#import db for image section, blob selection based on category input above.
 @app.route('/reference', methods=['GET','POST'])
 def reference():
     db = sqlite3.connect(MENUDB)
